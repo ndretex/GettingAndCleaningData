a6a1d56cf1812c16058f5f3f5b3629d5505cb579
ndretex <joelteixeira26@gmail.com>
2016-08-08 20:09:57 +0200

    correction of some errors

--- run_analysis.R
+++ run_analysis.R
@@ -15,21 +15,21 @@ if(!file.exists(datadir)) {
   unzip("project_dataset.zip")
 }
 
-##1 loading and merging train and tests sets for subject, activity and dataset
+# 1 loading and merging train and tests sets for subject, activity and dataset
 library(data.table)
-#loading subject sets
+## loading subject sets
 subjecttrain <- fread(file.path(datadir, "train", "subject_train.txt"))
 subjecttest <- fread(file.path(datadir, "test", "subject_test.txt"))
 subject <- rbind(subjecttrain,subjecttest)
 setnames(subject,"V1","subject")
 
-#loading activity sets
+## loading activity sets
 activitytrain <- fread(file.path(datadir, "train", "y_train.txt"))
 activitytest <- fread(file.path(datadir, "test", "y_test.txt"))
 activity <- rbind(activitytrain,activitytest)
 setnames(activity,"V1","numactivity")
 
-# loading descriptive activity names (3. descriptive activity names)
+## 3. loading descriptive activity names
 activitynames <- fread(file.path(datadir, "activity_labels.txt"))
 setnames(activitynames,c("V1","V2"),c("numactivity","activity"))
 
@@ -41,7 +41,7 @@ datatrain <- fread(file.path(datadir, "train", "x_train.txt"))
 datatest <- fread(file.path(datadir, "test", "x_test.txt"))
 data <- rbind(datatrain,datatest)
 
-# loading descriptive data variables names
+# 4 loading descriptive data variables names
 features <- fread(file.path(datadir, "features.txt"))
 featuresnames <- features$V2
 setnames(data,names(data),featuresnames)
@@ -49,11 +49,11 @@ setnames(data,names(data),featuresnames)
 dataset <- cbind(dataset,data)
 setkey(dataset,subject,activity)
 
-#2 Extracting only the measurements on the mean and standard deviation
+# 2 Extracting only the measurements on the mean and standard deviation
 
-# meanstdvars<-featuresnames[grepl("mean|std",namesdata)] 
-## This will keep the variables that contains "meanFreq()" which shouldn't be taking in consideration
-## i must then select the variables that contains "mean()" and "std()":
+### meanstdvars<-featuresnames[grepl("mean|std",namesdata)] 
+### This will keep the variables that contains "meanFreq()" which shouldn't be taking in consideration
+### i must then select the variables that contains "mean()" and "std()":
 meanstdvars<-featuresnames[grepl("mean\\(\\)|std\\(\\)",featuresnames)] 
 dataset<-dataset[,c("subject","activity",meanstdvars),with=FALSE]
 
@@ -61,6 +61,8 @@ dataset<-dataset[,c("subject","activity",meanstdvars),with=FALSE]
 dataset <- data.table(melt(dataset, key(dataset), variable.name="feature"))
 setkey(dataset,"subject","activity","feature")
 
-#5 independent tidy data set with the average of each variable for each activity and each subject.
+# 5 independent tidy data set with the average of each variable for each activity and each subject.
 tidydataset <- dataset[, list(average = mean(value)), by=key(dataset)]
 
+## output:
+write.table(tidydataset, file="tidydataset.txt", row.names = FALSE)
